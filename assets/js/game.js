bcc87d07c0fb3271dfb3b1f0aa3e3e366576a365
Matt Luna <mat.c.luna@gmail.com>
2021-07-06 19:15:01 -0500

    3.1.7 Finished

--- assets/js/game.js
+++ assets/js/game.js
@@ -18,7 +18,7 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function () {
-  window.alert("Welcome to Robot Gladiators" + playerName);
+  window.alert("Welcome to Robot Gladiators " + playerName);
 }
 
 fight();
@@ -29,5 +29,49 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
-var attackStrength = getRandomInt(playerAttack-3, playerAttack);
-console.log(attackStrength);
+var playerAttackStrength = getRandomInt(playerAttack - 3, playerAttack);
+console.log(playerAttackStrength);
+
+console.log("Enemy Health");
+console.log(enemyHealth);
+//Attack the enemy robot
+enemyHealth = enemyHealth - playerAttackStrength;
+
+//Enemy attack player
+var enemyAttackStrength = getRandomInt(enemyAttack - 3, enemyAttack);
+console.log(enemyAttackStrength);
+//Player Attacked
+playerHealth = playerHealth - enemyAttackStrength;
+
+//Console Log
+window.alert("The " + enemyName + " has " + enemyHealth + " health remaining. Your attack had a strength of " + playerAttackStrength + ".");
+console.log("Enemy Health after Attack");
+console.log(enemyHealth);
+console.log("Attack Strength");
+console.log(playerAttackStrength);
+
+window.alert(playerName + " has " + playerHealth + " health remaining. The enemy's attack had a strength of " + enemyAttackStrength + ".");
+console.log("Player Health after Attack");
+console.log(playerHealth);
+console.log("Enemy Attach Strength");
+console.log(enemyAttackStrength);
+
+//check to see if the value of the playerHealth variable is greater than 0
+if (playerHealth > 0) {
+  console.log("Your player is still alive!");
+}
+
+//Check player health
+if (playerHealth === 0) {
+  console.log("Your robot died.");
+}
+else {
+  console.log( playerName+" survived the robot battle!");
+}
+
+if (enemyHealth <= 0) {
+  console.log(enemyName + " has died!");
+}
+else {
+  console.log(enemyName + " still has " + enemyHealth + " health left.")
+}
