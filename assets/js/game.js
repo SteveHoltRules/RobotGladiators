d979107b7540e2224c0245d16f1d2f6ce9caf1e3
Matt Luna <mat.c.luna@gmail.com>
2021-07-06 23:26:30 -0500

    While loop for 3.1.8

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,27 +1,9 @@
-function fight() {
-  window.alert("The fight has begun!");
-}
-
-
-
-// fight();
-
 var playerName = window.prompt("What is your robot's name?");
-var playerHealth = 100;
-var playerAttack = 10;
+var playerHealth = 50;
+var playerAttack = 15;
+var playerMoney = 10;
 
 console.log(playerName);
-
-
-var enemyName = "Roborto";
-var enemyHealth = 50;
-var enemyAttack = 12;
-
-var fight = function () {
-  window.alert("Welcome to Robot Gladiators " + playerName);
-}
-
-fight();
 
 function getRandomInt(min, max) {
   min = Math.ceil(min);
@@ -29,49 +11,83 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
-var playerAttackStrength = getRandomInt(playerAttack - 3, playerAttack);
-console.log(playerAttackStrength);
-
-console.log("Enemy Health");
-console.log(enemyHealth);
-//Attack the enemy robot
-enemyHealth = enemyHealth - playerAttackStrength;
-
-//Enemy attack player
-var enemyAttackStrength = getRandomInt(enemyAttack - 3, enemyAttack);
-console.log(enemyAttackStrength);
-//Player Attacked
-playerHealth = playerHealth - enemyAttackStrength;
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
 //Console Log
-window.alert("The " + enemyName + " has " + enemyHealth + " health remaining. Your attack had a strength of " + playerAttackStrength + ".");
-console.log("Enemy Health after Attack");
-console.log(enemyHealth);
-console.log("Attack Strength");
-console.log(playerAttackStrength);
 
-window.alert(playerName + " has " + playerHealth + " health remaining. The enemy's attack had a strength of " + enemyAttackStrength + ".");
-console.log("Player Health after Attack");
-console.log(playerHealth);
-console.log("Enemy Attach Strength");
-console.log(enemyAttackStrength);
+var round = 1
 
-//check to see if the value of the playerHealth variable is greater than 0
-if (playerHealth > 0) {
-  console.log("Your player is still alive!");
+var fight = function () {
+  window.alert("Welcome to Robot Gladiators " + playerName);
+  console.log("Today's battle will be between "+ playerName + " and " + enemyName + ".")
+  //Ask to skip
+  var promptFight = window.confirm("Would you like to FIGHT (ok) or SKIP this battle (cancel)?")
+  //Confirm Skip
+  if (promptFight === false){
+    playerMoney =- 2;
+    console.log(playerMoney);
+    return;
+  }
+  else{
+    console.log(promptFight);
+  }
+  console.log(enemyName + " Health");
+  console.log(enemyHealth);
+  console.log(playerName + " Health");
+  console.log(playerHealth)
+  window.alert("Let the Fight Begin!");
+  onemore = true;
+//My loop is not re-evaluated with every pass. Why is that?
+  while (onemore === true && (playerHealth>0 && enemyHealth>0)){
+    //Attack Strength
+    var playerAttackStrength = getRandomInt(playerAttack - 7, playerAttack);
+    console.log(playerAttackStrength);
+
+    //Attack Strength
+    var enemyAttackStrength = getRandomInt(enemyAttack - 9, enemyAttack);
+    console.log(enemyAttackStrength);
+
+    //Enemy Attacked by player
+    console.log("Round " + round);
+    enemyHealth = enemyHealth - playerAttackStrength;
+
+    window.alert("The " + enemyName + " has " + enemyHealth + " health remaining. Your attack had a strength of " + playerAttackStrength + ".");
+    console.log("Enemy Health after Attack");
+    console.log(enemyHealth);
+    console.log("Attack Strength");
+    console.log(playerAttackStrength);
+    if (enemyHealth <= 0) {
+      console.log(enemyName + " has died!");
+      break;
+    }
+    else {
+      console.log(enemyName + " still has " + enemyHealth + " health left.");
+    }
+    //Player attacked by enemy
+    playerHealth = playerHealth - enemyAttackStrength;
+
+    window.alert(playerName + " has " + playerHealth + " health remaining. The enemy's attack had a strength of " + enemyAttackStrength + ".");
+    console.log(playerName + " Health after Attack");
+    console.log(playerHealth);
+    console.log(enemyName + " Attack Strength");
+    console.log(enemyAttackStrength);
+    if (playerHealth === 0) {
+      console.log(playerName + " died.");
+      break;
+    }
+    else {
+      console.log(playerName + " survived the robot battle!");
+    }
+    round++;
+    //Check player health
+    var onemore = window.confirm("Another round?");
+    console.log(onemore);
+    // if (onemore != true) {
+    //   break;
+    // }
+  }
 }
 
-//Check player health
-if (playerHealth === 0) {
-  console.log("Your robot died.");
-}
-else {
-  console.log( playerName+" survived the robot battle!");
-}
-
-if (enemyHealth <= 0) {
-  console.log(enemyName + " has died!");
-}
-else {
-  console.log(enemyName + " still has " + enemyHealth + " health left.")
-}
+fight();
